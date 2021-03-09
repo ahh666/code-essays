bfd311e0fc52978994607327b304f1383ac6f7aa
aihuanhuan <44722523+ahh666@users.noreply.github.com>
2021-03-09 15:51:06 +0800

    增加注释

--- draggable/index.js
+++ draggable/index.js
@@ -1,23 +1,26 @@
 /*
- * @Description: 拖动实现代码及思路
+ * @Description: 拖拽实现代码及思路
  * @Author: 艾欢欢<ahh666@qq.com>
  * @Date: 2021-03-09 11:02:22
- * @LastEditTime: 2021-03-09 14:52:36
+ * @LastEditTime: 2021-03-09 15:45:09
  * @LastEditors: 艾欢欢<ahh666@qq.com>
  * @FilePath: \js-essays\draggable\index.js
  */
 
 function dragHandler() {
-  // 用来存取被拖动的元素
+  // 用来存取被拖拽的节点
   let draggedNode = null
+  // 存放被拖拽节点的背景
   let draggedNodeBg = null
+  // 存放被拖拽节点进入的容器（节点） 的旧背景
   let dragEnterNodeBg = null
-  // 获取可拖动节点
+  // 获取可拖拽节点
   const dragNodes = document.querySelectorAll('div[draggable="true"]')
-  // 给可拖动节点绑定事件
+  // 给可拖拽节点绑定事件
   dragNodes.forEach(item => {
     item.ondragstart = () => {
       draggedNode = item
+      // 拖拽开始，设置背景
       draggedNodeBg = getComputedStyle(item).background
       item.style.background = '#bbb'
     }
@@ -25,9 +28,10 @@ function dragHandler() {
     item.ondragover = e => {
       e.preventDefault()
     }
-    // 被拖动的节点，被释放后进行节点替换
+    // 被拖拽的节点，被释放后进行节点替换
     item.ondrop = e => {
       if (dragEnterNodeBg) {
+        // 拖拽被释放，恢复目标节点背景
         e.target.style.background = dragEnterNodeBg
       }
       if (draggedNode !== null && draggedNode !== item) {
