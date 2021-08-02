0f394ed57ac65c1abade53c39a6d767bb833bfc4
aihuanhuan <44722523+ahh666@users.noreply.github.com>
2021-08-02 11:17:18 +0800

    节流升级
    根据参数判断是否执行最后一次动作

---
+++ throttle/index2.js
@@ -0,0 +1,38 @@
+/*
+ * @Description: Description
+ * @Author: 艾欢欢<ahh666@qq.com>
+ * @Date: 2021-08-02 10:55:38
+ * @LastEditTime: 2021-08-02 11:17:10
+ * @LastEditors: 艾欢欢<ahh666@qq.com>
+ * @FilePath: \js-essays\throttle\index2.js
+ */
+
+/**
+ * @description: 节流，立即执行，根据参数判断是否执行最后一次动作
+ * @param {*} fn
+ * @param {*} wait
+ * @param {*} trailing 用来决定是否执行最后一次但未达到一个时间周期的动作，默认不执行
+ * @return {*}
+ */
+function throttle(fn, wait = 300, trailing = false) {
+  let timer = null
+  let last = 0
+
+  function throttled() {
+    const context = this
+    let now = Date.now()
+    if (now - last > wait) {
+      if (timer) {
+        clearTimeout(timer)
+        timer = null
+      }
+      fn.apply(context, arguments)
+      last = now
+    } else if (trailing && !timer) {
+      timer = setTimeout(() => {
+        fn.apply(context, arguments)
+      }, wait)
+    }
+  }
+  return throttled
+}
